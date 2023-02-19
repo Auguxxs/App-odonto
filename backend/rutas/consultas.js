b4d5cd81577f4be0a99f6096371636e329747a58
Auguxxs <bleckmannaugusto@gmail.com>
2023-02-19 18:55:39 -0300

    mod + forum y creacion

--- backend/rutas/consultas.js
+++ backend/rutas/consultas.js
@@ -1,185 +1,61 @@
-const listaConsultas = document.getElementById("lista-consultas");
-const mascota = document.getElementById("mascota");
-const veterinaria = document.getElementById("veterinaria");
-const historia = document.getElementById("historia");
-const diagnostico = document.getElementById("diagnostico");
-const indice = document.getElementById("indice");
-const btnGuardar = document.getElementById("btn-guardar");
-const formulario = document.getElementById("formulario");
-
-let consultas = [];
-let pacientes = [];
-let veterinarias = [];
-
-const url = "https://veterinaria-backend.now.sh";
-
-async function listarConsultas() {
-  const entidad = "consultas";
-  try {
-    const respuesta = await fetch(`${url}/${entidad}`);
-    const consultasDelServidor = await respuesta.json();
-    if (Array.isArray(consultasDelServidor)) {
-      consultas = consultasDelServidor;
-    }
-    if (respuesta.ok) {
-      const htmlConsultas = consultas
-        .map(
-          (consulta, indice) =>
-            `<tr>
-          <th scope="row">${indice}</th>
-          <td>${consulta.mascota.nombre}</td>
-          <td>${consulta.veterinaria.nombre} ${consulta.veterinaria.apellido}</td>
-          <td>${consulta.diagnostico}</td>
-          <td>${consulta.fechaCreacion}</td>
-          <td>${consulta.fechaEdicion}</td>
-          
-          <td>
-              <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
-              </div>
-          </td>
-        </tr>`
-        )
-        .join("");
-      listaConsultas.innerHTML = htmlConsultas;
-      Array.from(document.getElementsByClassName("editar")).forEach(
-        (botonEditar, index) => (botonEditar.onclick = editar(index))
-      );
-    }
-  } catch (error) {
-    console.log({ error });
-    $(".alert-danger").show();
-  }
-}
-
-async function listarPacientes() {
-  const entidad = "pacientes";
-  try {
-    const respuesta = await fetch(`${url}/${entidad}`);
-    const pacientesDelServidor = await respuesta.json();
-    if (Array.isArray(pacientesDelServidor)) {
-      pacientes = pacientesDelServidor;
-    }
-    if (respuesta.ok) {
-      pacientes.forEach((_mascota, indice) => {
-        const optionActual = document.createElement("option");
-        optionActual.innerHTML = _mascota.nombre;
-        optionActual.value = indice;
-        mascota.appendChild(optionActual);
-      });
-    }
-  } catch (error) {
-    console.log({ error });
-    $(".alert-danger").show();
-  }
-}
-
-async function listarVeterinarias() {
-  const entidad = "veterinarias";
-  try {
-    const respuesta = await fetch(`${url}/${entidad}`);
-    const veterinariasDelServidor = await respuesta.json();
-    if (Array.isArray(veterinariasDelServidor)) {
-      veterinarias = veterinariasDelServidor;
-    }
-    if (respuesta.ok) {
-      veterinarias.forEach((_veterinaria, indice) => {
-        const optionActual = document.createElement("option");
-        optionActual.innerHTML = `${_veterinaria.nombre} ${_veterinaria.apellido}`;
-        optionActual.value = indice;
-        veterinaria.appendChild(optionActual);
-      });
-    }
-  } catch (error) {
-    console.log({ error });
-    $(".alert-danger").show();
-  }
-}
-
-function editar(index) {
-  return function cuandoCliqueo() {
-    btnGuardar.innerHTML = "Editar";
-    $("#exampleModalCenter").modal("toggle");
-    const consulta = consultas[index];
-    indice.value = index;
-    mascota.value = consulta.mascota.id;
-    veterinaria.value = consulta.veterinaria.id;
-    historia.value = consulta.historia;
-    diagnostico.value = consulta.diagnostico;
+module.exports = function consultasHandler({ consultas, nosotros, pacientes }) {
+  return {
+    get: (data, callback) => {
+      if (typeof data.indice !== "undefined") {
+        console.log("handler consultas", { data });
+        if (consultas[data.indice]) {
+          return callback(200, consultas[data.indice]);
+        }
+        return callback(404, {
+          mensaje: `consulta con indice ${data.indice} no encontrado `,
+        });
+      }
+      const consultasConRelaciones = consultas.map((consulta) => ({
+        ...consulta,
+        paciente: { ...pacientes[consulta.paciente], id: consulta.paciente },
+        nosotro: { ...nosotros[consulta.nosotros], id: consulta.nosotros },
+      }));
+      callback(200, consultasConRelaciones);
+    },
+    post: (data, callback) => {
+      let nuevaConsulta = data.payload;
+      nuevaConsulta.fechaCreacion = new Date();
+      (nuevaConsulta.fechaEdicion = null),
+        (consultas = [...consultas, nuevaConsulta]);
+      callback(201, nuevaConsulta);
+    },
+    put: (data, callback) => {
+      if (typeof data.indice !== "undefined") {
+        if (consultas[data.indice]) {
+          const { fechaCreacion } = consultas[data.indice];
+          consultas[data.indice] = {
+            ...data.payload,
+            fechaCreacion,
+            fechaEdicion: new Date(),
+          };
+          return callback(200, consultas[data.indice]);
+        }
+        return callback(404, {
+          mensaje: `consulta con indice ${data.indice} no encontrado`,
+        });
+      }
+      callback(400, { mensaje: "indice no enviado" });
+    },
+    delete: (data, callback) => {
+      if (typeof data.indice !== "undefined") {
+        if (consultas[data.indice]) {
+          consultas = consultas.filter(
+            (_consulta, indice) => indice != data.indice
+          );
+          return callback(204, {
+            mensaje: `elemento con indice ${data.indice} eliminado`,
+          });
+        }
+        callback(404, {
+          mensaje: `consulta con indice ${data.indice} no encontrado`,
+        });
+      }
+      callback(400, { mensaje: "indice no encontrado" });
+    },
   };
-}
-
-async function enviarDatos(evento) {
-  const entidad = "consultas";
-  evento.preventDefault();
-  try {
-    const datos = {
-      mascota: mascota.value,
-      veterinaria: veterinaria.value,
-      historia: historia.value,
-      diagnostico: diagnostico.value,
-    };
-    if (validar(datos) === true) {
-      const accion = btnGuardar.innerHTML;
-      let urlEnvio = `${url}/${entidad}`;
-      let method = "POST";
-      if (accion === "Editar") {
-        urlEnvio += `/${indice.value}`;
-        method = "PUT";
-      }
-      const respuesta = await fetch(urlEnvio, {
-        method,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(datos),
-        mode: "cors",
-      });
-      if (respuesta.ok) {
-        listarConsultas();
-        resetModal();
-      }
-      formulario.classList.add("was-validated");
-      return;
-    }
-    $(".alert-warning").show();
-  } catch (error) {
-    console.log({ error });
-    $(".alert-danger").show();
-  }
-}
-
-function resetModal() {
-  btnGuardar.innerHTML = "Crear";
-  [indice, mascota, veterinaria, historia, diagnostico].forEach(
-    (inputActual) => {
-      inputActual.value = "";
-      inputActual.classList.remove("is-invalid");
-      inputActual.classList.remove("is-valid");
-    }
-  );
-  $(".alert-warning").hide();
-  $("#exampleModalCenter").modal("toggle");
-}
-
-function validar(datos) {
-  if (typeof datos !== "object") return false;
-  let respuesta = true;
-  for (let llave in datos) {
-    if (datos[llave].length === 0) {
-      document.getElementById(llave).classList.add("is-invalid");
-      respuesta = false;
-    } else {
-      document.getElementById(llave).classList.remove("is-invalid");
-      document.getElementById(llave).classList.add("is-valid");
-    }
-  }
-  if (respuesta === true) $(".alert-warning").hide();
-  return respuesta;
-}
-
-btnGuardar.onclick = enviarDatos;
-
-listarPacientes();
-listarConsultas();
-listarVeterinarias();
+};
--- frontend/js/consultas.js
+++ frontend/js/consultas.js
@@ -1,21 +1,20 @@
 const listaConsultas = document.getElementById("lista-consultas");
+const obraSocial = document.getElementById("obra-social");
+const profesional = document.getElementById("profesional");
+const diagnostico = document.getElementById("diagnostico");
+const comentario = document.getElementById("comentario");
+const indice = document.getElementById("indice");
+const btnGuardar = document.getElementById("btn-guardar");
 
 let consultas = [];
-const url = "http://localhost:5000/consultas";
-
-/*  {
-      paciente: 0,
-      nosotros: 0,
-      fechaCreacion: new Date(),
-      fechaEdicion: new Date(),
-      historia: "",
-      diagnostico: "diagnostico",
-    },
-    */
+let obraSociales = [];
+let profesionales = [];
+const url = "http://localhost:5000";
 
 async function listarConsultas() {
+  const entidad = "consultas";
   try {
-    const respuesta = fetch(url);
+    const respuesta = fetch(`${url}/${entidad}`);
     const consultasDelServidor = await respuesta.json();
     if (Array.isArray(consultasDelServidor)) {
       consultas = consultasDelServidor;
@@ -27,14 +26,15 @@ async function listarConsultas() {
             `<tr>
             <th scope="row">2</th>
             <td>${consulta.paciente} </td>
-            <td>${consulta.nosotros} </td>
+            <td>${consulta.nosotros.nombre} ${consulta.nosotros.apellido}</td>
+            <td>${consulta.diagnostico} </td>
             <td>${consulta.fechaCreacion} </td>
             <td>${consulta.fechaEdicion} </td>
 
             <td>@fat</td>
             <td>
               <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-info">
+                <button class="editar" type="button" class="btn btn-info">
                   editar
                 </button>
               </div>
@@ -43,10 +43,106 @@ async function listarConsultas() {
         )
         .join("");
       listaConsultas.innerHTML = htmlConsultas;
+      Array.from(document.getElementsByClassName("editar")).forEach(
+        (botonEditar, index) => (botonEditar.onclick = editar(index))
+      );
     }
   } catch (error) {
     throw error;
   }
 }
 
-listarConsultas;
+async function listarObraSocial() {
+  const entidad = "obrasocial";
+  try {
+    const respuesta = fetch(`${url}/${entidad}`);
+    const obraSocialDelServidor = await respuesta.json();
+    if (Array.isArray(obraSocialDelServidor)) {
+      obraSociales = obraSocialDelServidor;
+    }
+    if (respuesta.ok) {
+      obraSocial.forEach((_obraSocial, indice) => {
+        const optionActual = document.createElement("option");
+        optionActual.innerHTML = _obraSocial.nombre;
+        optionActual.value = indice;
+        obraSocial.appendChild(optionActual);
+      });
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function listarProfesionales() {
+  const entidad = "profesionales";
+  try {
+    const respuesta = fetch(`${url}/${entidad}`);
+    const profesionalesDelServidor = await respuesta.json();
+    if (Array.isArray(profesionalesDelServidor)) {
+      profesionales = profesionalesDelServidor;
+    }
+    if (respuesta.ok) {
+      profesionales.forEach((_profesional, indice) => {
+        const optionActual = document.createElement("option");
+        optionActual.innerHTML = `${_profesional.nombre} ${_profesional.apellido}`;
+        optionActual.value = indice;
+        profesional.appendChild(optionActual);
+      });
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+function editar(index) {
+  return function cuandoCliqueo() {
+    btnGuardar.innerHTML = "Editar";
+    $("#exampleModalCenter").modal("toggle");
+    const consulta = consultas[index];
+    indice.value = index;
+    obraSocial.value = consulta.obraSocial.id;
+    profesional.value = consulta.profesional.id;
+    diagnostico.value = consulta.diagnostico;
+    comentario.value = consulta.comentario;
+  };
+}
+async function enviarDatos(evento) {
+  const entidad = "consultas";
+  evento.preventDefault();
+  try {
+    const datos = {
+      obraSocial: obraSocial.value,
+      profesional: profesional.value,
+      diagnostico: diagnostico.value,
+      comentario: comentario.value,
+    };
+    const accion = btnGuardar.innerHTML;
+    let urlEnvio = `${url}/${entidad}`;
+    let method = "POST";
+    if (accion === "Editar") {
+      urlEnvio += `/${indice.value}`;
+      method = "PUT";
+    }
+
+    const respuesta = await fetch(urlEnvio, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(datos),
+      mode: "cors",
+    });
+    if (respuesta.ok) {
+      listarConsultas();
+      // resetModal();
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+btnGuardar.onclick = enviarDatos;
+
+listarConsultas();
+listarObraSocial();
+listarProfesionales();
